5a54a3253e26f90a2f3ea7777c00eaa2676b1c9a
Phillip Oertel <me@phillipoertel.com>
2013-12-19 20:05:32 +0100

    Reduced some duplication

--- extract.rb
+++ extract.rb
@@ -35,37 +35,25 @@ def emails_for_folders(mail_root, folder_names)
   end.flatten
 end
 
+def build_list(name, mail_root, folders, exclude_patterns = [])
+  emails     = emails_for_folders(mail_root, folders)
+  old_emails = File.read("old_#{name}.txt").split("\n").map { |e| e.downcase }
+  emails     = emails + old_emails
+  emails.reject! do |email| 
+    exclude_patterns.any? { |exclude_pattern| email =~ /#{exclude_pattern}/ }
+  end
+  domains    = extract_domains(emails).uniq
+  write("#{name}.txt", domains)
+  puts "Wrote list '#{name}' with #{domains.size} entries."
+  domains
+end
+
 config = load_config
 
 START = Time.now
-#
-# blacklist
-#
-folders = config.blacklist_folders
-emails = emails_for_folders(config.mail_root, folders)
 
-old_emails = File.read('old_blacklist.txt').split("\n").map { |e| e.downcase }
-
-emails = emails + old_emails
-emails.reject! { |email| config.never_blacklist.any? { |never_blacklist_pattern| email =~ /#{never_blacklist_pattern}/ } }
-
-blacklist_domains = extract_domains(emails).uniq
-write('blacklist.txt', blacklist_domains)
-puts "Wrote blacklist with #{`wc -l blacklist.txt`.split.first.strip} entries."
-
-#
-# whitelist
-#
-folders = config.whitelist_folders
-emails = emails_for_folders(config.mail_root, folders)
-
-old_emails = File.read('old_whitelist.txt').split("\n").map { |e| e.downcase }
-
-emails = (emails + old_emails)
-
-whitelist_domains = extract_domains(emails).uniq
-write('whitelist.txt', whitelist_domains)
-puts "Wrote whitelist with #{`wc -l whitelist.txt`.split.first.strip} entries."
+blacklist_domains = build_list(:blacklist, config.mail_root, config.blacklist_folders, config.never_blacklist)
+whitelist_domains = build_list(:whitelist, config.mail_root, config.whitelist_folders)
 
 # check if domains are on both whitelist and blacklist
 intersection = blacklist_domains & whitelist_domains
